49bbb0f632cc16ca6ba14697910774fdb366a8c2
David Tinianow <david.tinianow@gmail.com>
2016-09-12 10:06:16 -0600

    Able to access all stations in one location through the search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
     location = params[:q]
-    token = ENV['nrel_token']
-    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&api_key=#{token}")
-    # JSON.parse(response.body, symbolize_name: true, object_class: OpenStruct)
+    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&api_key=#{ENV['nrel_token']}")
+    response = connection.get
+    @locations = JSON.parse(response.body, symbolize_name: true, object_class: OpenStruct)
+    require "pry"; binding.pry
   end
 end
