2ce69bb187dcc4bdced60514bd7bd572b36e26fc
David Tinianow <david.tinianow@gmail.com>
2016-09-12 09:59:19 -0600

    Refactor search controller index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController
   def index
-
     location = params[:q]
-    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&api_key=#{ENV[nrel_key]}")
-    connection.headers["Authorization"] = "token #{user.token}"
-    JSON.parse(response.body, symbolize_name: true, object_class: OpenStruct)
+    token = ENV['nrel_token']
+    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&api_key=#{token}")
+    # JSON.parse(response.body, symbolize_name: true, object_class: OpenStruct)
   end
 end
