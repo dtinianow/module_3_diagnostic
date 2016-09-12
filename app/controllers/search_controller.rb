73fb90af0e0e53f55c49031ddb161bad4cd39de2
David Tinianow <david.tinianow@gmail.com>
2016-09-12 10:12:58 -0600

    Add all search parameters to api url

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController
   def index
     location = params[:q]
-    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&api_key=#{ENV['nrel_token']}")
+    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&radius=6.0&fuel_type=ELEC,LPG&api_key=#{ENV['nrel_token']}")
     response = connection.get
     @locations = JSON.parse(response.body, symbolize_name: true, object_class: OpenStruct)
-    require "pry"; binding.pry
   end
 end
