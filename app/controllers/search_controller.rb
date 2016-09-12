23af82e4519e09ee7046a134bcc638a7e96a042e
David Tinianow <david.tinianow@gmail.com>
2016-09-12 09:52:41 -0600

    Add api url to controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
-   def index
-     
-   end
+  def index
+
+    location = params[:q]
+    connection = Faraday.new("http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{location}&api_key=#{ENV[nrel_key]}")
+    connection.headers["Authorization"] = "token #{user.token}"
+    JSON.parse(response.body, symbolize_name: true, object_class: OpenStruct)
+  end
 end
